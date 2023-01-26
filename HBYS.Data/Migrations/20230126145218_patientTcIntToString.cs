fe46b966d227cd1524b5be050f3587473c16c487
Kadircan Arıcı <kadircanarc@gmail.com>
2023-01-26 18:37:50 +0300

    tc ve tel no gereklilikleri sağlandı.

---
+++ HBYS.Data/Migrations/20230126145218_patientTcIntToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HBYS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class patientTcIntToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TC",
+                table: "Patients",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TC",
+                table: "Patients",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+        }
+    }
+}
--- HBYS.Models/Patient.cs
+++ HBYS.Models/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,8 @@ namespace HBYS.Models
     [Table("Patients")]
     public class Patient:ModelBase
     {
-        public int TC { get; set; }
+        [MaxLength(11)]
+        public string TC { get; set; }
         public string? Name { get; set; }
         public string? Phone { get; set; }
         public string? Address { get; set; }
